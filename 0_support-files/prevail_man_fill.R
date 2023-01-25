2b6fa470278cd4f0e1fef3e705958f8c179cdeb7
Conor Loy <31441382+conorloy@users.noreply.github.com>
2023-01-25 01:26:36 -0500

    Add files via upload

---
+++ 0_support-files/prevail_man_fill.R
@@ -0,0 +1,10 @@
+theme_PREVAIL <- function(){
+  scale_fill_manual(values  = c('Control \nn=4'="#D86050",
+                                'COVID-19 \nNonsevere \nn=30'="#F89088",
+                                'COVID-19 \nSevere \nn=22'="firebrick1",
+                                'Control \nn=5'="#D86050",
+                                'COVID-19 \n acute \nn=18'="springgreen2",
+                                'MIS-C \n acute \nn=40'="steelblue1"))
+            
+}
+
